966fb97a8627cb63418eb999662f490d0bb2d776
clairox <33704909+clairox@users.noreply.github.com>
2025-08-25 17:57:06 -0400

    feat(gui): change Box color to show contrast

--- src/mpy3/gui/core.py
+++ src/mpy3/gui/core.py
@@ -1,22 +1,22 @@
 import pygame
 
-from mpy3.gui.widgets import Box, Button, Screen, Vector
+from mpy3.gui.widgets import Box, Button, Canvas, Vector
 
 
 class App:
     def run(self):
-        screen = Screen()
+        canvas = Canvas()
         play_button = Button("Play")
         pause_button = Button("Pause")
         container = Box(
             {
-                "size": Vector(screen.screen.get_width(), 60),
-                "background_color": pygame.Color("#000000"),
+                "size": Vector(canvas.buffer.get_width(), 60),
+                "background_color": pygame.Color("gray"),
             }
         )
-        screen.add_widget(play_button)
-        screen.add_widget(pause_button)
-        screen.add_widget(container)
+        canvas.add_widget(play_button)
+        canvas.add_widget(pause_button)
+        canvas.add_widget(container)
 
         pygame.init()
 
@@ -26,7 +26,7 @@ class App:
                 if event.type == pygame.QUIT:
                     running = False
 
-            screen.update()
+            canvas.update()
 
             pygame.display.flip()
 
