6cc54d0bde30d4c04840a1f41fe55a9ca1731dbd
clairox <33704909+clairox@users.noreply.github.com>
2025-08-26 00:49:47 -0400

    feat(app): load media files on startup

--- src/mpy3/__main__.py
+++ src/mpy3/__main__.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 from mpy3.gui.core import App
+
+DEFAULT_MEDIA_DIR = Path.home() / "mpy3/tracks/"
 
 
 def main():
-    App().run()
+    media_dir = Path(DEFAULT_MEDIA_DIR)
+    if not media_dir.exists():
+        Path.mkdir(media_dir, parents=True)
+
+    app = App(media_dir)
+    app.run()
 
 
 if __name__ == "__main__":
--- src/mpy3/gui/core.py
+++ src/mpy3/gui/core.py
@@ -1,25 +1,53 @@
+import os
+from pathlib import Path
+
 import pygame
 
-from mpy3.gui.widgets import Box, Button, Canvas
+from mpy3.gui.widgets import Box, Button, Canvas, Text
+from mpy3.player import Media
 
 
 class App:
-    def run(self) -> None:
-        canvas = Canvas()
-        play_button = Button("Play")
-        pause_button = Button("Pause")
-        container = Box(
-            {
-                "width": canvas.buffer.get_width(),
-                "height": 60,
-                "background_color": pygame.Color("gray"),
-            }
-        )
-        canvas.add_widget(play_button)
-        canvas.add_widget(pause_button)
-        canvas.add_widget(container)
+    def __init__(self, media_dir: Path):
+        self.media_dir = media_dir
 
+        unsorted_media_list = []
+        for mrl in self.media_dir.iterdir():
+            media = Media(mrl)
+            media.parse_meta()
+            unsorted_media_list.append(media)
+
+        self.media_list = sorted(unsorted_media_list, key=lambda media: media.title)
+
+    def run(self) -> None:
         pygame.init()
+        canvas = Canvas()
+        clock = pygame.time.Clock()
+
+        if len(self.media_list) == 0:
+            canvas.add_widget(Text("No music"))
+        else:
+            track_list_container = Box({"width": canvas.buffer.get_width()})
+
+            for media in self.media_list:
+                label = ""
+                if media.meta:
+                    label = f'{media.title} - {media.meta["artist"]}'
+                else:
+                    label = media.title
+
+                track_list_item = Box(
+                    {
+                        "width": track_list_container.get_width(),
+                        "border_bottom_size": 2,
+                    }
+                )
+
+                track_list_item_text = Text(label)
+                track_list_item.add_widget(track_list_item_text)
+                track_list_container.add_widget(track_list_item)
+
+            canvas.add_widget(track_list_container)
 
         running = True
         while running:
@@ -30,5 +58,6 @@ class App:
             canvas.update()
 
             pygame.display.flip()
+            clock.tick(60)
 
         pygame.quit()
